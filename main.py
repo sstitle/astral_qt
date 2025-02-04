dbaa7903bf3749c73e6efd3deef713343a6a0a24
Sam Title <samstitle@gmail.com>
2025-02-04 00:43:03 -0500

    Using vertex buffers

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ from OpenGL.GL import *
 from OpenGL.GLU import * 
 from OpenGL.GLUT import *
 from PyQt6.QtCore import Qt
-from PyQt6.QtCore import Qt
 from PyQt6.QtOpenGLWidgets import QOpenGLWidget
 from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QSlider, QLabel, QWidget
-
 
 # Constants
 NUM_CUBES = 1000
@@ -27,7 +25,7 @@ MIN_DISTANCE_FROM_CAMERA = 2.0  # Minimum distance from the camera
 SPEED_MIN = 0.1
 SPEED_MAX = 1.0
 DIRECTION_RANGE = 1.0
-LINE_WIDTH = 2.0
+LINE_WIDTH = 4.0
 PERSPECTIVE_ANGLE = 45.0
 PERSPECTIVE_NEAR = 1.0
 PERSPECTIVE_FAR = 100.0
@@ -48,6 +46,7 @@ class GLWidget(QOpenGLWidget):
         self.cube_directions = self.generate_random_directions(self.num_cubes)
         self.camera_angle = 0.0  # Initialize camera angle
         self.camera_angle_speed = CAMERA_ANGLE_SPEED
+        self.vbo = None
 
     def generate_random_positions(self, num_cubes):
         # Generate positions around the camera position (0, 0, CAMERA_INITIAL_Z)
@@ -73,6 +72,68 @@ class GLWidget(QOpenGLWidget):
         glEnable(GL_DEPTH_TEST)
         glLineWidth(LINE_WIDTH)
 
+        # Define cube vertices
+        vertices = np.array([
+            # Front face
+            [-CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE],
+            [CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE],
+            [CUBE_SIZE, CUBE_SIZE, CUBE_SIZE],
+            [-CUBE_SIZE, CUBE_SIZE, CUBE_SIZE],
+            # Back face
+            [-CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE],
+            [CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE],
+            [CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE],
+            [-CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE],
+            # Top face
+            [-CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE],
+            [CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE],
+            [CUBE_SIZE, CUBE_SIZE, CUBE_SIZE],
+            [-CUBE_SIZE, CUBE_SIZE, CUBE_SIZE],
+            # Bottom face
+            [-CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE],
+            [CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE],
+            [CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE],
+            [-CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE],
+            # Right face
+            [CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE],
+            [CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE],
+            [CUBE_SIZE, CUBE_SIZE, CUBE_SIZE],
+            [CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE],
+            # Left face
+            [-CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE],
+            [-CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE],
+            [-CUBE_SIZE, CUBE_SIZE, CUBE_SIZE],
+            [-CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE]
+        ], dtype=np.float32)
+
+        # Define cube edges
+        edges = np.array([
+            # Front face
+            [0, 1], [1, 2], [2, 3], [3, 0],
+            # Back face
+            [4, 5], [5, 6], [6, 7], [7, 4],
+            # Top face
+            [3, 2], [2, 6], [6, 7], [7, 3],
+            # Bottom face
+            [0, 1], [1, 5], [5, 4], [4, 0],
+            # Right face
+            [1, 2], [2, 6], [6, 5], [5, 1],
+            # Left face
+            [0, 3], [3, 7], [7, 4], [4, 0],
+            # Connecting front and back faces
+            [0, 4], [1, 5], [2, 6], [3, 7]
+        ], dtype=np.uint32)
+
+        # Create a VBO for vertices
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+        # Create a VBO for edges
+        self.ebo = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.ebo)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, edges.nbytes, edges, GL_STATIC_DRAW)
+
     def resizeGL(self, w, h):
         glViewport(0, 0, w, h)
         glMatrixMode(GL_PROJECTION)
@@ -85,87 +146,35 @@ class GLWidget(QOpenGLWidget):
         glLoadIdentity()
 
         # Rotate the camera around the scene
-        self.camera_angle += self.camera_angle_speed  # Use the variable for camera angle speed
+        self.camera_angle += self.camera_angle_speed
         glRotatef(self.camera_angle, 0.0, 1.0, 0.0)
         glTranslatef(0.0, 0.0, CAMERA_INITIAL_Z)
 
-        self.cube_angles += self.cube_speeds  # Increment the rotation angle for each cube
+        self.cube_angles += self.cube_speeds
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glVertexPointer(3, GL_FLOAT, 0, None)
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.ebo)
 
         for i, (x, y, z) in enumerate(self.cube_positions):
             glPushMatrix()
             glTranslatef(x, y, z)
             glRotatef(self.cube_angles[i], *self.cube_directions[i])
             glColor3f(*self.cube_colors[i])
-            self.drawBox()
+            glDrawArrays(GL_QUADS, 0, 24)
+            glColor3f(*EDGE_COLOR)
+            glLineWidth(2.0)
+            # Ensure the correct number of elements is specified
+            glDrawElements(GL_LINES, 48, GL_UNSIGNED_INT, None)  # Corrected to 48 elements
             glPopMatrix()
 
+        glDisableClientState(GL_VERTEX_ARRAY)
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
+
         self.update()
-
-    def drawBox(self):
-        glBegin(GL_QUADS)
-        # Front face
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        # Back face
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        # Top face
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        # Bottom face
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        # Right face
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        # Left face
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        glEnd()
-
-        # Draw edges
-        glColor3f(*EDGE_COLOR)  # Set edge color to black
-        glBegin(GL_LINES)
-        # Front face edges
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        # Back face edges
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        # Connecting edges
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-        glVertex3f(-CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE)
-        glEnd()
 
     def set_camera_angle_speed(self, speed):
         self.camera_angle_speed = speed / 100.0  # Adjust speed based on slider value
